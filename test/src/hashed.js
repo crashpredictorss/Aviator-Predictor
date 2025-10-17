a2e7620971e617ca213d0d3564622ed742cd2a13
crashpredictorss <asddad419@outlook.com>
2025-10-17 08:40:55 +0300

    Add files via upload

---
+++ test/src/hashed.js
@@ -0,0 +1,67 @@
+
+(function (global, factory) {
+  typeof exports === "object" && typeof module !== "undefined"
+    ? (module.exports = factory())
+    : typeof define === "function" && define.amd
+    ? define(factory)
+    : ((global = global || self), (global.hashData = factory()));
+})(this, function () {
+  "use strict";
+
+  /* This is used often so make a global copy that everything can reference */
+  const millionaAscii = "a".repeat(1000000);
+
+  const hash_data = {
+    "SHA-1": [
+      {
+      
+        name: "Long Data With Long Customization With Long Key",
+        input: {
+          format: "HEX",
+          value:
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F808182838485868788898A8B8C8D8E8F909192939495969798999A9B9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7",
+        },
+        customization: {
+          format: "TEXT",
+          value:
+            "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+        },
+        kmacKey: {
+          format: "TEXT",
+          value:
+            "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+        },
+        outputs: [
+          {
+            format: "HEX",
+            value:
+              "4c2cca887c7e632062a421c574f8b9cd26b55d1ede7747e7ebc4d0e32f4e5f06814e439a5c0fc26c9312b7e0e4185cd7aedf54292c79480ca854e61a48b72f3c",
+            outputLen: 512,
+          },
+        ],
+      },
+      {
+        name: "Long Data Without Customization",
+        input: {
+          format: "HEX",
+          value:
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F808182838485868788898A8B8C8D8E8F909192939495969798999A9B9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7",
+        },
+        kmacKey: {
+          format: "HEX",
+          value: "404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F",
+        },
+        outputs: [
+          {
+            format: "HEX",
+            value:
+              "75358cf39e41494e949707927cee0af20a3ff553904c86b08f21cc414bcfd691589d27cf5e15369cbbff8b9a4c2eb17800855d0235ff635da82533ec6b759b69",
+            outputLen: 512,
+          },
+        ],
+      },
+    ],
+  };
+
+  return hash_data;
+});
